3fdefdd0244732d7df4b61ef08f3badfd8612eed
Vinícius Alexandre <viniciosalexandre312@gmail.com>
2025-10-23 00:58:08 -0300

    tirando as portas 8080

--- frontend/src/services/recipeEditService.js
+++ frontend/src/services/recipeEditService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function atualizarReceita(id, formData) {
-  const response = await axios.put(`https://backend-pi-20-production.up.railway.app:8080/receitas/${id}/update`, formData, {
+  const response = await axios.put(`https://backend-pi-20-production.up.railway.app/receitas/${id}/update`, formData, {
     headers: {
     },
   });
@@ -10,11 +10,11 @@ export async function atualizarReceita(id, formData) {
 }
 
 export async function buscarReceita(id) {
-    const response = await axios.get(`https://backend-pi-20-production.up.railway.app:8080/receitas/${id}`);
+    const response = await axios.get(`https://backend-pi-20-production.up.railway.app/receitas/${id}`);
     return response.data;
 }
 
 export function deletarReceita(id) {
-    axios.delete(`https://backend-pi-20-production.up.railway.app:8080/receitas/${id}`);
+    axios.delete(`https://backend-pi-20-production.up.railway.app/receitas/${id}`);
 }
   
--- frontend/src/services/recipeFindItensService.js
+++ frontend/src/services/recipeFindItensService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://backend-pi-20-production.up.railway.app:8080/receitas';
+const API_BASE_URL = 'https://backend-pi-20-production.up.railway.app/receitas';
 
 export const getAllRecipes = async () => {
   const response = await axios.get(API_BASE_URL);
