18798c8f52a84335bcd7ed31b96b06f6d78063b2
Vinícius Alexandre <107442699+Vinicius2543@users.noreply.github.com>
2025-10-23 20:11:29 -0300

    Atualizando o CORS

--- backend/ChefNotes/src/main/java/com/backend/projectbank/config/CustomCorsFilter.java
+++ backend/ChefNotes/src/main/java/com/backend/projectbank/config/CustomCorsFilter.java
@@ -21,7 +21,7 @@ public class CustomCorsFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "https://projeto-pi-tau.vercel.app");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Allow-Credentials", "true");
